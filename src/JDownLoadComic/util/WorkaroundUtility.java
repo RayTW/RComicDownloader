942a309f9028c0ab0621bbd4fa85b39bb11d0b97
Ray <ray00000sina@gmail.com>
2015-08-28 23:35:17 +0800

    fixed bug : 去除已下載漫畫資料夾半形空白時，若讀取到系統資料夾會crash。

--- src/JDownLoadComic/util/WorkaroundUtility.java
+++ src/JDownLoadComic/util/WorkaroundUtility.java
@@ -7,9 +7,12 @@ import JDownLoadComic.Config;
 public class WorkaroundUtility {
 	// 存放所有集數的資料夾，例如" 海賊王"，前後若有半形空白trim掉
 	public static void replaceAllSpaceCharComic() {
-		File flist = new File("./" + Config.defaultSavePath);
+		File[] flist = new File("./" + Config.defaultSavePath).listFiles();
+		if (flist == null) {
+			return;
+		}
 
-		for (File f : flist.listFiles()) {
+		for (File f : flist) {
 			if (f.isDirectory() && f.getName().indexOf(' ') != -1) {
 				f.renameTo(new File(f.getParentFile().getPath(), f.getName()
 						.replaceAll(" ", "")));
@@ -19,10 +22,19 @@ public class WorkaroundUtility {
 
 	// 存放一集或1話的資料夾，例如" 第一卷"，前後若有半形空白trim掉
 	public static void replaceAllSpaceCharAct() {
-		File flist = new File("./" + Config.defaultSavePath);
+		File[] flist = new File("./" + Config.defaultSavePath).listFiles();
+		if (flist == null) {
+			return;
+		}
 
-		for (File comic : flist.listFiles()) {
-			for (File f : comic.listFiles()) {
+		File[] comicList = null;
+
+		for (File comic : flist) {
+			comicList = comic.listFiles();
+			if (comicList == null) {
+				continue;
+			}
+			for (File f : comicList) {
 				if (f.isDirectory() && f.getName().indexOf(' ') != -1) {
 					f.renameTo(new File(f.getParentFile().getPath(), f
 							.getName().replaceAll(" ", "")));
