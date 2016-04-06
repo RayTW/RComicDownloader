a5b130ff64f08c945f942b703ba841bf44c4d7aa
Ray <ray00000sina@gmail.com>
2016-04-06 22:20:53 +0800

    Revision : v2.0.1

--- src/net/xuite/blog/ray00000test/rdownloadcomic/service/Config.java
+++ src/net/xuite/blog/ray00000test/rdownloadcomic/service/Config.java
@@ -18,7 +18,7 @@ import net.xuite.blog.ray00000test.rdownloadcomic.util.URLEncodeDecodeTool;
 
 public class Config {
 	/** 版本 */
-	public static final String version = "v2.0.0";
+	public static final String version = "v2.0.1";
 	// 首頁的title
 	public static String indexName = "漫畫下載%s \t 程式設計: Ray";
 
